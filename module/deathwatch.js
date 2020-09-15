c766a199bd547de51f9c0f7049945f84143026b6
marco <marco.schroeder96@gmail.com>
2020-09-15 21:16:56 +0200

    Add bonus to characteristics template and init formula
    Added the characteristics bonus, which is currently not updated
    Added initiative formula for characters

--- module/deathwatch.js
+++ module/deathwatch.js
@@ -16,8 +16,8 @@ Hooks.once('init', async function() {
    * @type {String}
    */
   CONFIG.Combat.initiative = {
-    formula: "1d20",
-    decimals: 2
+    formula: "1d10 + @characteristics.ag.bonus",
+    decimals: 0
   };
 
   // Define custom Entity classes
